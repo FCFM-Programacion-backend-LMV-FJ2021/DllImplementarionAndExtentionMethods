1a583a5042e27f061dc38a38fb055a6d80dd6c1c
Rolando <rolando.fcfm2020@gmail.com>
2021-02-20 16:52:44 -0600

    first commit

---
+++ TestFunctions_LMV/NewFunctions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyFunctionsDWB_LMV;
+
+namespace TestFunctions_LMV
+{
+    public static class NewFunctions
+    {
+        public static decimal Div(this Functions fn, decimal a, decimal b)
+        {
+            return a / b;
+        }
+
+        public static decimal Resta(this Functions funct, decimal a, decimal b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ TestFunctions_LMV/Program.cs
@@ -0,0 +1,32 @@
+﻿using MyFunctionsDWB_LMV;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFunctions_LMV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal x = 23;
+            decimal y = 41;
+
+            Functions fn = new Functions();
+            var sum = fn.Sum(x, y);
+            var mult = fn.Mult(x, y);
+            var div = fn.Div(x, y);
+            var resta = fn.Resta(x, y);
+
+            Console.WriteLine("La suma es: " + sum);
+            Console.WriteLine("La multiplicación es: " + mult);
+            Console.WriteLine("La división es: " + div);
+            Console.WriteLine("La resta es: " + resta);
+
+            Console.ReadLine();
+
+        }
+    }
+}
